50cb6dc33122ef820b5f8073bb584a48436e277d
simran02-08-2023 <singhanchal33141@gmail.com>
2026-07-02 16:34:38 +0530

    Intersectio of two sorted arrays

---
+++ Arrays/src/Intersection.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Intersection {
+    static ArrayList<Integer> intersection(int []a, int [] b){
+        int n1=a.length;
+        int n2=b.length;
+        int i=0, j=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+        while (i < n1 && j < n2) {
+            if (a[i] <b[j]) i++;
+            else if(b[j]<a[i]) j++;
+            else {
+                ans.add(a[i]);
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size and elements of first sorted array:");
+        int n1 = sc.nextInt();
+        int[] a = new int[n1];
+
+        for (int i = 0; i < n1; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter size and elements of second sorted array:");
+        int n2 = sc.nextInt();
+        int[] b = new int[n2];
+
+        for (int i = 0; i < n2; i++) {
+            b[i] = sc.nextInt();
+        }
+
+        ArrayList<Integer> ans = intersection(a, b);
+        System.out.println("Intersection of both arrays is: " + ans);
+    }
+}
